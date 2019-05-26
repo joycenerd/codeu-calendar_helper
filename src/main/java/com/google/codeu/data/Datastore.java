029209e7b530bd403e3db980e319f553f462ae2d
graceHunag <dyna.ghieds12388@gmail.com>
2019-05-26 11:15:36 +0800

    add css & stats page

--- src/main/java/com/google/codeu/data/Datastore.java
+++ src/main/java/com/google/codeu/data/Datastore.java
@@ -23,6 +23,7 @@ import com.google.appengine.api.datastore.PreparedQuery;
 import com.google.appengine.api.datastore.Query;
 import com.google.appengine.api.datastore.Query.FilterOperator;
 import com.google.appengine.api.datastore.Query.SortDirection;
+import com.google.appengine.api.datastore.FetchOptions;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.UUID;
@@ -45,7 +46,11 @@ public class Datastore {
 
     datastore.put(messageEntity);
   }
-
+public int getTotalMessageCount(){
+  Query query = new Query("Message");
+  PreparedQuery results = datastore.prepare(query);
+  return results.countEntities(FetchOptions.Builder.withLimit(1000));
+}
   /**
    * Gets messages posted by a specific user.
    *
