dbd39a66f49832d2483063d8bf5b511dd1200db1
xuau34 <btd84860@gmail.com>
2019-05-27 15:18:22 +0800

    bootstraped feed page

--- src/main/webapp/js/feed.js
+++ src/main/webapp/js/feed.js
@@ -20,26 +20,34 @@ function fetchMessages(){
 
 function buildMessageDiv(message){
   const usernameDiv = document.createElement('div');
-  usernameDiv.classList.add("left-align");
+  usernameDiv.classList.add('col', 'text-left', 'p-2');
   usernameDiv.appendChild(document.createTextNode(message.user));
 
-  const timeDiv = document.createElement('div');
-  timeDiv.classList.add('right-align');
+  const timeDiv = document.createElement('small');
+  timeDiv.classList.add('col', 'text-right', 'p-2');
   timeDiv.appendChild(document.createTextNode(new Date(message.timestamp)));
 
   const headerDiv = document.createElement('div');
-  headerDiv.classList.add('message-header');
+  headerDiv.classList.add('row', 'border-bottom');
   headerDiv.appendChild(usernameDiv);
   headerDiv.appendChild(timeDiv);
 
+  const textDiv = document.createElement('div');
+  textDiv.classList.add('col', 'text-justify', 'p-2');
+  textDiv.appendChild(document.createTextNode(message.text));
+
   const bodyDiv = document.createElement('div');
-  bodyDiv.classList.add('message-body');
-  bodyDiv.appendChild(document.createTextNode(message.text));
+  bodyDiv.classList.add('row');
+  bodyDiv.appendChild(textDiv);
+
+  const messageInnerDiv = document.createElement('div');
+  messageInnerDiv.classList.add('col', 'container');
+  messageInnerDiv.appendChild(headerDiv);
+  messageInnerDiv.appendChild(bodyDiv);
 
   const messageDiv = document.createElement('div');
-  messageDiv.classList.add("message-div");
-  messageDiv.appendChild(headerDiv);
-  messageDiv.appendChild(bodyDiv);
+  messageDiv.classList.add("row", "border-0", "rounded", "shadow", "mb-3", "bg-light");
+  messageDiv.appendChild(messageInnerDiv);
 
   return messageDiv;
 }
