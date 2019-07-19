e0404309577c7da509c1918d8a58aec81127a855
xuau34 <btd84860@gmail.com>
2019-07-19 17:08:13 +0800

    Completed updating tags

--- src/main/webapp/js/dashboard.js
+++ src/main/webapp/js/dashboard.js
@@ -372,15 +372,28 @@ function initSamplePopover(){
   });
   $("#table-submit").click(function( event ){
       event.preventDefault();
-      var startDateTime = new Date( $('#startDateTime [type="hidden"]').val() );
-      var endDateTime   = new Date( $('#endDateTime [type="hidden"]').val() );
+      var eventData = $('#table-form').serializeArray();
+      var startDateTime = new Date( eventData.find(o => o.name === 'startDateTime').value );
+      var endDateTime   = new Date( eventData.find(o => o.name === 'endDateTime').value );
       if( startDateTime >= endDateTime ){
         alert("Invalid date time.");
         return;
       }
-      $.post("/dashboard/calendar", $("#table-form").serializeArray(), function(data){
+      $.post("/dashboard/calendar", eventData, function(data){
           console.log(data);
           if(data.error != null) window.location.replace(data.to);
+          var tagData = {
+            method: 'update',
+            eventDateTime: eventData.find(o => o.name === 'startDateTime').value
+          };
+          for( const tag of eventData.find(o => o.name === 'tags').value.split(/[\s,]+/) ){
+            tagData.tag = tag;
+            console.log(tagData);
+            $.post("/dashboard/tagManage", tagData, function(response){
+                console.log(response);
+                if(response.error != null) window.location.replace(data.to);
+                });
+          };
           $('#table-form').focusout();
           loadTimetable();    //Here should be modified to adding animatedly
           }, "json")
