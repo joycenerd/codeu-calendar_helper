aefec6f599a8988aa402febffb2f2cc26d563723
ChungYujoyce <46030368+ChungYujoyce@users.noreply.github.com>
2019-07-20 10:51:25 +0800

    Update dashboard.js
    put those code back

--- src/main/webapp/js/dashboard.js
+++ src/main/webapp/js/dashboard.js
@@ -370,4 +370,35 @@ function initSamplePopover(){
         $('#table-sample-end').text( date.toLocaleTimeString("default", options) );
       }
   });
+  $("#table-submit").click(function( event ){
+      event.preventDefault();
+      var eventData = $('#table-form').serializeArray();
+      var startDateTime = new Date( eventData.find(o => o.name === 'startDateTime').value );
+      var endDateTime   = new Date( eventData.find(o => o.name === 'endDateTime').value );
+      if( startDateTime >= endDateTime ){
+        alert("Invalid date time.");
+        return;
+      }
+      $.post("/dashboard/calendar", eventData, function(data){
+          console.log(data);
+          if(data.error != null) window.location.replace(data.to);
+          var tagData = {
+            method: 'update',
+            eventDateTime: eventData.find(o => o.name === 'startDateTime').value
+          };
+          for( const tag of eventData.find(o => o.name === 'tags').value.split(/[\s,]+/) ){
+            tagData.tag = tag;
+            console.log(tagData);
+            $.post("/dashboard/tagManage", tagData, function(response){
+                console.log(response);
+                if(response.error != null) window.location.replace(data.to);
+                });
+          };
+          $('#table-form').focusout();
+          loadTimetable();    //Here should be modified to adding animatedly
+          }, "json")
+      .fail(function(err){
+          console.log(err);
+          });
+      });
 }
