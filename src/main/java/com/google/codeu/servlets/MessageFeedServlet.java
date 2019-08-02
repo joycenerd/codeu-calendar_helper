0467535f27e8e973dbefb9928f81283f0afdb586
Jun Chen <juncheng@juncheng-macbookpro.roam.corp.google.com>
2019-08-02 11:33:29 +0800

    Fix hash tag search

--- src/main/java/com/google/codeu/servlets/MessageFeedServlet.java
+++ src/main/java/com/google/codeu/servlets/MessageFeedServlet.java
@@ -4,7 +4,10 @@ import com.google.codeu.data.Datastore;
 import com.google.codeu.data.Message;
 import com.google.gson.Gson;
 import java.io.IOException;
+import java.net.URLDecoder;
+import java.util.Iterator;
 import java.util.List;
+import java.util.regex.Pattern;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
@@ -29,7 +32,13 @@ public class MessageFeedServlet extends HttpServlet {
   public void doGet(HttpServletRequest request, HttpServletResponse response) throws IOException {
     response.setContentType("application/json");
 
+    // Get the filter entered by the user.
+    String filter = request.getParameter("filter");
+
     List<Message> messages = datastore.getAllMessages();
+    if (filter != null && filter.length() > 0) {
+      messages.removeIf(message -> !message.getText().matches(Pattern.quote(filter)));
+    }
     Gson gson = new Gson();
     String json = gson.toJson(messages);
 
