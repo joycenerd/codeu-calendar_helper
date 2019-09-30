6238548b16ebba749752b720fc40d89b193d134c
joycenerd <joycenerd@csie.io>
2019-09-30 09:12:01 +0800

    fix error after moving the original project

--- src/main/java/com/google/codeu/servlets/ImageAnalysisServlet.java
+++ src/main/java/com/google/codeu/servlets/ImageAnalysisServlet.java
@@ -1,3 +1,5 @@
+package com.google.codeu.servlets;
+
 import com.google.appengine.api.blobstore.BlobInfo;
 import com.google.appengine.api.blobstore.BlobInfoFactory;
 import com.google.appengine.api.blobstore.BlobKey;
